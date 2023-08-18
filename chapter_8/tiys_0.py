1ce11aa0f8ae0ef4b707ecc4b0187e0966f69caa
tford-dev <terranceford513@gmail.com>
2023-08-18 11:57:34 -0400

    Finished optional argument and nested function example

---
+++ chapter_8/tiys_0.py
@@ -0,0 +1,30 @@
+def get_formatted_name(first_name, last_name, middle_name=''):
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}";
+    else:
+         full_name = f"{first_name} {last_name}";
+    
+    return full_name.title();
+
+musician = get_formatted_name('kanye', 'west', 'omari')
+# print(musician);
+
+def name_of_user():
+    while True:
+        print("Please, tell me your government name:");
+        print("(Enter 'q' at any time to quit, you coward.)");
+        first_name = input("First name: ");
+        if (first_name.lower() == 'q'):
+            print("Process aborted.");
+            break;
+        last_name = input("Last name: ");
+        if (last_name.lower() == 'q'):
+            print("Process aborted.");
+            break;
+        middle_name = input("*Middle name is optional* Enter 's' if you wish to skip. \nMiddle name: ")
+        if ((middle_name.lower() == 's') or (middle_name.lower() == '')):
+            middle_name = None;
+        formatted_name = get_formatted_name(first_name, last_name, middle_name);
+        print(f"You have shown bravery, {formatted_name}.");
+
+name_of_user();
